e3dcf3857432e7fa20cca9e6be40af522ac68213
ymmtmdk <ymmtmdk@gmail.com>
2017-03-29 20:22:21 +0900

    Update bobthefish_color.fish

--- bobthefish_color.fish
+++ bobthefish_color.fish
@@ -13,26 +13,25 @@ set -l white    ffffff
 set -l black    000000
 set -l ruby_red af0000
 
-set __color_initial_segment_exit     $white $red[2] --bold
-set __color_initial_segment_su       $white $green[2] --bold
-set __color_initial_segment_jobs     $white $blue[3] --bold
+set -g __color_initial_segment_exit     $white $red[2] --bold
+set -g __color_initial_segment_su       $white $green[2] --bold
+set -g __color_initial_segment_jobs     $white $blue[3] --bold
 
-set __color_path                     $grey[3] $grey[2]
-set __color_path_basename            $grey[3] $white --bold
-set __color_path_nowrite             $red[3] $red[1]
-set __color_path_nowrite_basename    $red[3] $red[1] --bold
+set -g __color_path                     $grey[3] $grey[2]
+set -g __color_path_basename            $grey[3] $white --bold
+set -g __color_path_nowrite             $red[3] $red[1]
+set -g __color_path_nowrite_basename    $red[3] $red[1] --bold
 
-set __color_repo                     $green[1] $green[3]
-set __color_repo_work_tree           $green[1] $white --bold
-set __color_repo_dirty               $red[2] $white
-set __color_repo_staged              $orange[1] $orange[3]
+set -g __color_repo                     $green[1] $green[3]
+set -g __color_repo_work_tree           $green[1] $white --bold
+set -g __color_repo_dirty               $red[2] $white
+set -g __color_repo_staged              $orange[1] $orange[3]
 
-set __color_vi_mode_default          $grey[2] $grey[3] --bold
-set __color_vi_mode_insert           $green[2] $grey[3] --bold
-set __color_vi_mode_visual           $orange[1] $orange[3] --bold
+set -g __color_vi_mode_default          $grey[2] $grey[3] --bold
+set -g __color_vi_mode_insert           $green[2] $grey[3] --bold
+set -g __color_vi_mode_visual           $orange[1] $orange[3] --bold
 
-set __color_vagrant                  $blue[1] $white --bold
-set __color_username                 $grey[1] $blue[3]
-set __color_rvm                      $ruby_red $grey[1] --bold
-set __color_virtualfish              $blue[2] $grey[1] --bold
-
+set -g __color_vagrant                  $blue[1] $white --bold
+set -g __color_username                 $grey[1] $blue[3]
+set -g __color_rvm                      $ruby_red $grey[1] --bold
+set -g __color_virtualfish              $blue[2] $grey[1] --bold
