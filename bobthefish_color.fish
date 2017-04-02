2a26aa1cdb8409aa991629abaffc0f020e4b5974
ymmtmdk <ymmtmdk@gmail.com>
2017-04-02 13:48:42 +0900

    Update bobthefish_color.fish

--- bobthefish_color.fish
+++ bobthefish_color.fish
@@ -2,6 +2,7 @@
 function bobthefish_color
   # fish_prompt
   set -g theme_display_date no
+  set -g theme_display_user yes
   set -g theme_color_scheme user
   #               light  medium dark
   #               ------ ------ ------
